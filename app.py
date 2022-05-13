e2855f1423653ed40ebc197c37858e0e8f620fe1
Paolo Zanotti <60442209+zanottipaolo@users.noreply.github.com>
2022-05-13 17:52:15 +0200

    ✅ Add sprint on app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('login.html')
 
+@app.route('/sprint', methods=['POST', 'GET'])
+def sprint():
+    return render_template('sprint.html')
+
 if __name__ == '__main__': 
      app.run(debug=True)
