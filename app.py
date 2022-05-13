eee977d70cda92feccff219b02cead3516ec2025
Paolo Zanotti <60442209+zanottipaolo@users.noreply.github.com>
2022-05-13 18:34:06 +0200

    ✏️ Fix login && base section

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    return render_template('login.html')
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
 
 @app.route('/sprint', methods=['POST', 'GET'])
 def sprint():
