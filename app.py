84999c5f8029d0bdc5a09b63e4391e3a238bb407
Jfkmdd <federico.deduro@gmail.com>
2022-05-15 23:32:04 +0200

    Update BE & FE of Backlog

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from backend.models import Sprint, Task
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -15,28 +16,43 @@ def login():
     else:
         return render_template('login.html')
 
+
 @app.route('/sprint', methods=['POST', 'GET'])
 def sprint():
     sprint = Sprint.query.all()
     return render_template('sprint.html', sprint=sprint)
+
 
 @app.route('/backlog', methods=['POST', 'GET'])
 def backlog():
     if request.method == 'GET':
         tasks = Task.query.all()
         sprints = Sprint.query.all()
-        return render_template('backlog/backlog.html',tasks=tasks, sprints=sprints)
-    else:
+        return render_template('backlog/backlog.html', tasks=tasks, sprints=sprints)
+    if request.method == 'POST' and 'create-new-task' in request.form:
         # add new task
-        new_task = Task(request.form.get('name'), request.form.get('description'))
+        new_task = Task(request.form.get('name'),
+                        request.form.get('description'))
         db_session.add(new_task)
         db_session.commit()
         return redirect('/backlog')
+    if request.method == 'POST' and 'task-delete' in request.form:
+        # delete task
+        app.logger.info("DELETE TASK")
+        Task.query.filter_by(id=request.form.get('idTaskToDelete')).delete()
+        db_session.commit()
+        return redirect('/backlog')
+    if request.method == 'POST' and 'task-changed' in request.form:
+        # delete task
+        app.logger.info("CHAGE TASK")
+        return redirect('/backlog')
+    # Close connection to database when shutting down
 
-# Close connection to database when shutting down
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
-if __name__ == '__main__': 
-     app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
