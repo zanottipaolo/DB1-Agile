f50656a5a2d24701ee5e70610cb282fdca4600f4
Jfkmdd <federico.deduro@gmail.com>
2022-05-16 12:30:06 +0200

    Add move and remove from sprint in backlog

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from webbrowser import get
 from flask import Flask, redirect, render_template, request
+from sqlalchemy import func
 
 # Database
 from backend.database import db_session
@@ -29,11 +30,12 @@ def backlog():
     if request.method == 'GET':
         tasks = Task.query.all()
         sprints = Sprint.query.all()
-        current_sprint = Sprint.query.filter_by(is_active=1).first()
+        current_sprint = Sprint.query.filter_by(is_active=1).one()
         backlog_task = Task.query.filter_by(sprint=None)
         sprint_task = Task.query.filter_by(sprint=current_sprint.id)
         epics = Epic.query.all()
-        return render_template('backlog/backlog.html', tasks=tasks, sprints=sprints, current_sprint=current_sprint, backlog_task=backlog_task, sprint_task=sprint_task, epics=epics)
+        total_points_of_sprint = 100  # Fare SUM di sprint_task.fibonacci_points
+        return render_template('backlog/backlog.html', tasks=tasks, sprints=sprints, current_sprint=current_sprint, backlog_task=backlog_task, sprint_task=sprint_task, epics=epics, total_points_of_sprint=total_points_of_sprint)
     if request.method == 'POST' and 'create-new-task' in request.form:
         # add new task
         new_task = Task(request.form.get('name'),
@@ -49,17 +51,39 @@ def backlog():
     if request.method == 'POST' and 'task-delete' in request.form:
         # delete task
         app.logger.info("DELETE TASK")
-        Task.query.filter_by(id=request.form.get('idTaskToDelete')).delete()
+        Task.query.filter_by(id=request.form.get('idTask')).delete()
         db_session.commit()
         return redirect('/backlog')
     if request.method == 'POST' and 'task-changed' in request.form:
-        # delete task
-        app.logger.info("CHAGE TASK")
+        # changed task
+        app.logger.info("CHANGE TASK")
+        task_to_change = Task.query.get(request.form.get('idTask'))
+        task_to_change.name = request.form.get('name')
+        task_to_change.description = request.form.get('description')
+        # reporter
+        task_to_change.fibonacci_points = request.form.get(
+            'fibonacci_points_info')
+        db_session.commit()
+        return redirect('/backlog')
+    if request.method == 'POST' and 'remove-from-sprint' in request.form:
+        # Remove task from sprint
+        app.logger.info("REMOVE TASK FROM SPRINT")
+        task_to_remove = Task.query.get(request.form.get('idTask'))
+        task_to_remove.sprint = None
+        db_session.commit()
+        return redirect('/backlog')
+    if request.method == 'POST' and 'task-move-in-sprint' in request.form:
+        # move task in current sprint
+        app.logger.info("MOVE TASK IN CURRENT SPRINT")
+        task_to_move = Task.query.get(request.form.get('idTask'))
+        current_sprint = Sprint.query.filter_by(is_active=1).one()
+        task_to_move.sprint = current_sprint.id
+        db_session.commit()
         return redirect('/backlog')
     # Close connection to database when shutting down
 
 
-@app.teardown_appcontext
+@ app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
