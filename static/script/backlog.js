8f89b4f600aa00b1e9ae0321b09c5dc31576015e
Jfkmdd <federico.deduro@gmail.com>
2022-05-18 12:58:33 +0200

    Update backlog js

--- static/script/backlog.js
+++ static/script/backlog.js
@@ -1,6 +1,4 @@
-function get_sprint_task() {
-    return JSON.parse('{{ sprint_task|tojson }}');
-}
+
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -11,22 +9,31 @@ function drag(ev, idTask, is_in_sprint) {
 }
   
 function drop_on_backlog(ev) {
-    console.log("DROPPED ON BACKLOG");
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text/html");//retrieves dropped images data.
-    console.log("ID task moved: " + data);
+    var id_task_to_move = ev.dataTransfer.getData("text/html");//retrieves dropped images data.
+    currentPosition = document.getElementById(id_task_to_move).parentNode.id; // Current sprint or backlog
 
-    alert("Move in backlog?")
+    if (currentPosition == "backlog") {
+        // è già presente in backlog
+        console.log('Task già presente in sprint');
+        return;
+    }
+    else {
+        console.log('Da spostare');
+    }
 }
 
 function drop_on_sprint(ev) {
-    console.log("DROPPED ON SPRINT ");
     ev.preventDefault();
     var id_task_to_move = ev.dataTransfer.getData("text/html");//retrieves dropped images data.
-    console.log("ID task moved: " + get_sprint_task());
+    var currentPosition = document.getElementById(id_task_to_move).parentNode.parentNode.id; // Current sprint or backlog
 
-    if (get_sprint_task().includes(id_task_to_move)) {
+    if (currentPosition == "currentSprint") {
         // è già presente in sprint
+        console.log('Task già presente in sprint');
         return;
+    }
+    else {
+        console.log('Da spostare');
     }
 } 
